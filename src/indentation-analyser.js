5192f2fd7b8574571aaaecb99be158fae0223b63
Pouya Kary <k@karyfoundation.org>
2016-08-30 02:50:22 +0430

    string skip + total indentation ...

--- src/indentation-analyser.js
+++ src/indentation-analyser.js
@@ -20,33 +20,82 @@
             let bracketsIndentation = 0;
             let curlyBracketsIndentation = 0;
 
+            let currentLine = 0;
+            let lastAddedLine = -1;
+
+            let totalIndentation = 0;
+
         //
         // ─── BODY ────────────────────────────────────────────────────────
         //
 
             for ( let index = 0; index < code.length; index++ ) {
+                // defs
                 let character = code[ index ];
 
+                // to make our life much more easy...
+                function skipString ( literal ) {
+                    let onScapeSequenceSign = false;
+                    while ( index < code.length ) {
+                        character = code[ index ];
+                        if ( character === literal ) {
+                            if ( onScapeSequenceSign === false ) {
+                                return;
+                            } else {
+                                onScapeSequenceSign = false;
+                            }
+                        } else if ( character === '\\' ) {
+                            onScapeSequenceSign = true;
+                        }
+                    }
+                }
+
+                // incrementing indentation requires much care for cases like
+                // .forEach( something => {..., in one line ( and { are detected
+                // by line only requires one level of indentation...
+                function incrementNewIndentation ( ) {
+                    if ( currentLine > lastAddedLine ) {
+                        totalIndentation++;
+                        lastAddedLine = currentLine;
+                    }
+                }
+
+
+                // body
                 switch ( character ) {
-                    case '(':
-                        parenthesesIndentation++;
-                        break;
-                    case ')':
-                        parenthesesIndentation--;
+
+                    case '\n':
+                        currentLine++;
                         break;
 
+                    case '(':
+                        parenthesesIndentation++;
+                        incrementNewIndentation( );
+                        break;
                     case '[':
                         bracketsIndentation++;
+                        incrementNewIndentation( );
+                        break;
+                    case '{':
+                        curlyBracketsIndentation++;
+                        incrementNewIndentation( );
+                        break;
+
+
+                    case ')':
+                        parenthesesIndentation--;
                         break;
                     case ']':
                         bracketsIndentation--;
                         break;
-
-                    case '{':
-                        curlyBracketsIndentation++;
-                        break;
                     case '}':
                         curlyBracketsIndentation--;
+                        break;
+
+                    case "'":
+                    case '"':
+                    case '`':
+                        skipString( character );
                         break;
                 }
             }
