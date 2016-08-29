fff2954c8dda553f9c43b67bc578c507993f9062
Pouya Kary <k@karyfoundation.org>
2016-08-30 03:47:48 +0430

    empty line fixer...

---
+++ src/finalizer.js
@@ -0,0 +1,16 @@
+
+//
+// Righteous - Kary Foundation Styled Code formatter
+//     Copyright 2016 By Kary Foundation, Inc.
+//     Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── MAIN ───────────────────────────────────────────────────────────────────────
+//
+
+    module.exports = code => {
+        return code.replace( /^\s+$/gm, '\r\n' );
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
