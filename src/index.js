c86f71522c80623c833b3c8ffe14a4fc5dfcf112
Pouya Kary <k@karyfoundation.org>
2017-08-27 11:52:38 +0430

    updating for the next version

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    const ecmascriptFormatter = require('./typescript/index.js' )
+    const cssFormatter = require( './css/index.js' )
+
+//
+// ─── INDEX ──────────────────────────────────────────────────────────────────────
+//
+
+    return {
+        ecmascriptFormatter,
+        cssFormatter
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
